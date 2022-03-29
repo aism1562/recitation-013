48bae23970b47863af378c66cef5c158384bea9b
aism1562 <89609659+aism1562@users.noreply.github.com>
2022-03-29 10:23:34 -0600

    Js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,64 @@
-const para = document.querySelector('p')
+// const para = document.querySelector('p');
+//
+// para.addEventListener('click', updateName);
+//
+// function updateName () {
+//   const name = prompt('Enter a new name');
+//   para.textContent = `Player 1: ${name}`;
+// }
+//
 
-para.addEventListener()
+// let  myVariable;
+// myVariable = 'Foo';
+//
+// let myVariable2 = 'Fon';
+//
+// myVariable = 'Celine';
+
+/*This is a
+multi-line comment */
+
+//this is a comment
+
+// VARIABLES
+/*
+let myVariable = 'bob'; // STRINGS
+
+let myNumber = 3; // NUMBERS
+
+let myBoolean = true; // BOOLEANS
+let myBoolean = false;
+
+let myArray = [1, 'bob', true]; // ARRAYS
+
+let myObject = document.querySelector('p');
+// OBJECTS, BUT ALL THE ABOVE ARE OBJECTS TOO
+
+// OPERATORS
+
+6 + 9; // ADDITION
+'Hello' + 'World'; // STRING ADDITION --> 'HelloWorld'
+
+// SUBTRACTION --> -
+// MULTIPLICATION --> *
+// DIVISION --> /
+
+// ASSIGNMENT --> =
+
+// EQUALITY
+myNumber === 4; // <-- This will result in false
+
+// NOT, DOES-NOT-EQUAL
+!(myNumber === 3); // !(TRUE) = FALSE;
+myVariable !== 3; // FALSE
+
+*/
+
+// CONDITIONALS
+
+let iceCream= 'chocolate';
+if(iceCream === 'chocolate') {
+  alert('Yay, I love chocolate ice cream!');
+} else {
+  alert('die');
+}
