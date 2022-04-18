7347b457a6d48f16c2d0e0db62cfd8d457a650c9
aism1562 <89609659+aism1562@users.noreply.github.com>
2022-04-17 19:47:48 -0600

    Added CSS for assignment 13

--- js/assignment-13.js
+++ js/assignment-13.js
@@ -11,28 +11,38 @@ const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. W
 const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 const insertY = ["the soup kitchen", "Disneyland", "the White House"];
 const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const stonesConversionRate = 0.071428571428571
+const weightInPounds = 300
+const tempInF = 94
 
 randomize.addEventListener('click', result);
 
 function result() {
 
-const newStory = storyText;
+  let newStory = storyText;
+  let xItem = randomValueFromArray(insertX)
+  let yItem = randomValueFromArray(insertY)
+  let zItem = randomValueFromArray(insertZ)
 
-xItem = randomValueFromArray(insertX)
-yItem = randomValueFromArray(insertY)
-zItem = randomValueFromArray(insertZ)
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':insertx:', xItem);
+  newStory = newStory.replace(':inserty:', yItem);
+  newStory = newStory.replace(':insertz:', zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    newStory = newStory.replace('Bob', name);
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = Math.round(weightInPounds * stonesConversionRate) + ' stone';
+    let fToC = (5/9) * (tempInF - 32)
+    const temperature =  Math.round(fToC) + ' centigrade';
+    newStory = newStory.replace('94 fahrenheit', temperature);
+    newStory = newStory.replace('300 pounds', weight);
 
   }
 
-  story.textContent = ;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
